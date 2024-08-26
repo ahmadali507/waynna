8c76bf81bccb331cffc2ffae6c1a8caf7dd711a0
ahmadali507 <ashahid.bese23seecs@seecs.edu.pk>
2024-08-27 02:35:23 +0500

    minor changes

--- src/app/_layout/cta.tsx
+++ src/app/_layout/cta.tsx
@@ -17,59 +17,59 @@ export const Cta: React.FC = () => {
           </p>
         </div>
 
-        <div className="relative mx-auto flex h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] flex-col gap-6 sm:gap-8 overflow-hidden rounded-2xl sm:rounded-3xl bg-[#0C0B0B] text-white lg:w-full xl:h-[90vh]">
+        <div className="relative mx-auto flex h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] flex-col gap-6 sm:gap-8 overflow-hidden rounded-2xl sm:rounded-3xl bg-[#0C0B0B] text-white lg:w-full xl:h-[80vh]">
           <div className="relative px-4 py-4 sm:py-6 lg:absolute lg:left-[5%] lg:top-[20%] lg:w-1/3 space-y-4 sm:space-y-6">
             <h3 className="text-xl sm:text-2xl font-bold leading-tight sm:leading-snug lg:text-3xl xl:text-4xl xl:w-[20rem] xl:leading-[56px]">
               Let&#39;s Hala <span className="text-[#C9FC8C]">W</span>aynaa and start your adventure.
             </h3>
 
-            <div className="z-40 flex flex-col sm:flex-row items-center gap-3">
+            <div className="z-40 flex flex-col sm:flex-row items-center gap-3 ">
               <Link
                 href="#"
-                className="flex items-start gap-2 rounded-full border border-lime-400 px-4 py-2 shadow-[0_0_54px_rgba(180,247,47,0.2)]"
+                className="flex items-start gap-2 rounded-full border border-lime-400 px-4 py-2 shadow-[0_0_54px_rgba(180,247,47,0.2)] h-9"
               >
                 <Image
                   src="/apple.svg"
                   alt="App store link"
-                  height={20}
-                  width={20}
+                  height={14}
+                  width={16}
                 />
                 <div className="flex flex-col justify-start gap-0.5">
                   <Image
                     src="/apple-download.svg"
                     alt="Download"
-                    width={76}
-                    height={7}
+                    width={73}
+                    height={17}
                   />
                   <Image
                     src="/app-store.svg"
                     alt="App store"
-                    width={81}
+                    width={61}
                     height={17}
                   />
                 </div>
               </Link>
               <Link
                 href="#"
-                className="flex items-start gap-2 rounded-full border border-lime-400 px-4 py-2 shadow-[0_0_54px_rgba(180,247,47,0.2)]"
+                className="flex items-start gap-2 rounded-full border border-lime-400 px-4 py-2 shadow-[0_0_54px_rgba(180,247,47,0.2)] h-9 w-36"
               >
                 <Image
                   src="/google.svg"
                   alt="App store link"
-                  height={24}
-                  width={20}
+                  height={14}
+                  width={16}
                 />
                 <div className="flex flex-col justify-start gap-0.5">
                   <Image
                     src="/google-download.svg"
                     alt="Download"
-                    width={43}
+                    width={33}
                     height={7}
                   />
                   <Image
                     src="/google-play.svg"
                     alt="App store"
-                    width={81}
+                    width={61}
                     height={17}
                   />
                 </div>
@@ -78,15 +78,24 @@ export const Cta: React.FC = () => {
           </div>
           <div className="relative h-full w-full flex justify-center items-center lg:justify-between">
             {/* Background Screen */}
+            <div className=" absolute z-10 border-2 h-[108vh] opacity-10 border-1 -right-[18vw] top-[10vh] w-[60vw] rounded-full">
+
+            </div>
+            
+            <div className=" absolute z-10 border-2 h-[88vh] opacity-10 border-1 right-[2vw] top-[30vh] w-[35vw] rounded-full border-dashed border-spacing-4">
+            </div>
+            
+            <div className=" absolute z-30 border-2 h-[68vh] opacity-10  left-[69vh] border-1 top-[43vh] w-[20vw] rounded-full  border-spacing-4">
+            </div>
             <Image
-              className="z-10 w-[40%] sm:w-[30%] md:w-[25%] lg:w-[30%] h-auto lg:absolute lg:bottom-0 lg:left-72 lg:h-[30rem] xl:absolute xl:left-[25rem]"
+              className="z-10 w-[40%] sm:w-[30%] md:w-[25%] lg:w-[30%] h-auto lg:absolute lg:bottom-0 lg:left-72 lg:h-[30rem] xl:absolute xl:left-[29rem] xl:h-[60vh]"
               src={screenBack}
               alt="Background usage screen"
             />
             
             {/* Foreground Screen */}
             <Image
-              className="z-20 w-[50%] sm:w-[35%] lg:w-[45%] xl:w-[35%] h-auto lg:absolute lg:right-[6%] lg:bottom-0 lg:h-[100vh] xl:absolute xl:right-[6%]
+              className="z-20 w-[50%] sm:w-[35%] lg:w-[45%] xl:w-[35%] h-auto lg:absolute lg:right-[6%] lg:bottom-0 lg:h-[100vh] xl:absolute xl:right-[2%] xl:h-[75vh]
               "
               src={screenFront}
               alt="Foreground usage screen"
--- src/app/_layout/footer.tsx
+++ src/app/_layout/footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="lg:py-8 xl:px-12">
-      <div className="flex flex-col items-center justify-between gap-6 bg-[#0C0B0B] py-8 text-white lg:flex-row lg:rounded-full lg:px-[100px] xl:px-[270px]">
-        <p className="text-sm sm:text-base">
+    <footer className="lg:py-8 xl:px-12 ">
+      <div className="flex flex-col items-center justify-center bg-[#0C0B0B] py-8 text-white lg:flex-row lg:rounded-full lg:px-[100px] xl:px-[50px] h-16 xl:gap-x-64">
+        <p className="text-[11px] text-gray-200">
           Waynaa&copy;2024. All rights reserved!
         </p>
         <div className="flex gap-6">
@@ -13,26 +13,26 @@ export const Footer: React.FC = () => {
             <Image
               src="/instagram.svg"
               alt="Instagram"
-              width={24}
-              height={24}
+              width={18}
+              height={18}
             />
           </Link>
 
           <Link href="#">
-            <Image src="/youtube.svg" alt="YouTube" width={24} height={24} />
+            <Image src="/youtube.svg" alt="YouTube" width={18} height={18} />
           </Link>
 
           <Link href="#">
-            <Image src="/linkedin.svg" alt="Linkedin" width={24} height={24} />
+            <Image src="/linkedin.svg" alt="Linkedin" width={18} height={18} />
           </Link>
 
           <Link href="#">
-            <Image src="/x.svg" alt="X" width={24} height={24} />
+            <Image src="/x.svg" alt="X" width={18} height={18} />
           </Link>
         </div>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center text-xs gap-4 text-[11px] text-gray-200">
           Sponsored by{" "}
-          <Image src="/borgfy.svg" alt="Borgfy logo" height={32} width={53} />
+          <Image src="/borgfy.svg" alt="Borgfy logo" height={24} width={43} />
         </div>
       </div>
     </footer>
